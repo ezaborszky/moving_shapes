ca6f96324495f773f3430dd109f3e52c0709879a
ezaborszky <124446749+ezaborszky@users.noreply.github.com>
2023-09-01 21:07:25 +0200

    modified:   circle.h 	modified:   main.cpp 	new file:   rectangle.h

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 #include <fstream>
 #include <SFML/Graphics.hpp>
 #include "circle.h"
-
+#include "rectangle.h"
 using std::string;
 
 
@@ -14,13 +14,18 @@ int main() {
     const int wWidth = 1024;
     const int wHeight = 768; 
     
+    std::vector<Circle> circles;
+
     sf::RenderWindow window(sf::VideoMode(wWidth, wHeight), "Shapes");
     window.setFramerateLimit(60);
 
     
     Circle myCircle;
     Circle myCircle2("Circle2", 200, 250, 5.f, 3.f, 20, 100, 90, 20);
+    Rectangle myRectangle("Rect", 100, 600, 5, 5, 200, 100, 30, sf::Vector2f(200,100));
 
+    circles.push_back(myCircle);
+    circles.push_back(myCircle2);
    
 
     while (window.isOpen())
@@ -32,14 +37,26 @@ int main() {
                 window.close();
         }
 
-    window.clear();
+    window.clear(sf::Color(100,100,5));
  
-    myCircle.draw(window);
-    myCircle.detectCol(wWidth, wHeight);
-    myCircle2.draw(window);
-    myCircle2.detectCol(wWidth, wHeight);
-    myCircle2.move();
-    myCircle.move();
+
+    for(auto &c : circles) 
+    {
+        c.draw(window);
+        c.detectCol(wWidth, wHeight);
+        c.move();
+    };
+
+    // myCircle.draw(window);
+    // myCircle.detectCol(wWidth, wHeight);
+    // myCircle2.draw(window);
+    // myCircle2.detectCol(wWidth, wHeight);
+    // myCircle2.move();
+    // myCircle.move();
+
+    myRectangle.draw(window);
+    myRectangle.detectCol(wWidth, wHeight);
+    myRectangle.move();
     window.display();
 
     }
--- rectangle.h
+++ rectangle.h
@@ -2,7 +2,7 @@
 
 using std::string;
 
-class Circle {
+class Rectangle {
 
     string m_name = "Default";
     int m_xPos = 300;
@@ -10,37 +10,37 @@ class Circle {
     float m_xVel = 5.5f;
     float m_yVel = 5.5f;
     std::vector<int> m_rgb = {0,100,25};
-    float m_size = 50.f;
+    sf::Vector2f m_size = sf::Vector2f(16.5f, 24.f);
     sf::Font m_font;
-    int m_charSize = m_size / 2;
+    int m_charSize = m_size.y / 2;
 
 public:
 
-    Circle() 
+    Rectangle() 
     {
-        circle.setRadius(m_size);
-        circle.setFillColor(sf::Color(m_rgb[0], m_rgb[1], m_rgb[2]));
+        rectangle.setSize(m_size);
+        rectangle.setFillColor(sf::Color(m_rgb[0], m_rgb[1], m_rgb[2]));
         m_font.loadFromFile("font.ttf");
         text.setFont(m_font);
         text.setString(m_name);
-        circle.setPosition(m_xPos, m_yPos);
-        text.setPosition(m_xPos + (m_size/4), m_yPos + (m_size/2) + m_charSize/2);
+        rectangle.setPosition(m_xPos, m_yPos);
+        text.setPosition(m_xPos + (m_size.x/4), m_yPos + (m_size.y/2) + m_charSize/2);
         text.setCharacterSize(m_charSize);
     }
 
-    Circle(float size) 
+    Rectangle(sf::Vector2f size) 
     : m_size(size)
     {
-        circle.setRadius(m_size);
+        rectangle.setSize(m_size);
         m_font.loadFromFile("font.ttf");
         text.setFont(m_font);
         text.setString(m_name);
-        circle.setPosition(m_xPos, m_yPos);
-        text.setPosition(m_xPos + (m_size/4), m_yPos + (m_size/2) + m_charSize/2);
+        rectangle.setPosition(m_xPos, m_yPos);
+        text.setPosition(m_xPos + (m_size.x/4), m_yPos + (m_size.y/2) + m_charSize/2);
         text.setCharacterSize(m_charSize);
     }
 
-    Circle(string name, int xpos, int ypos, float xvel, float yvel, int r, int g, int b, float size)
+    Rectangle(string name, int xpos, int ypos, float xvel, float yvel, int r, int g, int b, sf::Vector2f size)
     : m_name(name)
     , m_xPos(xpos)
     , m_yPos(ypos)
@@ -50,49 +50,48 @@ public:
     , m_size(size)
     
     {
-        circle.setRadius(m_size);
-        circle.setFillColor(sf::Color(m_rgb[0], m_rgb[1], m_rgb[2]));
+        rectangle.setSize(m_size);
+        rectangle.setFillColor(sf::Color(m_rgb[0], m_rgb[1], m_rgb[2]));
         m_font.loadFromFile("font.ttf");
         text.setFont(m_font);
         text.setString(m_name);
-        circle.setPosition(m_xPos, m_yPos);
-        text.setPosition(m_xPos + (m_size/4), m_yPos + (m_size/2) + m_charSize/2);
+        rectangle.setPosition(m_xPos, m_yPos);
+        text.setPosition(m_xPos + (m_size.x/4), m_yPos + (m_size.y/4));
         text.setCharacterSize(m_charSize);
     }
 
     void draw(sf::RenderWindow &window)
     {
-        window.draw(circle);
+        window.draw(rectangle);
         window.draw(text);
     }
 
     void move() 
     {
-        circle.setPosition(circle.getPosition().x + m_xVel, circle.getPosition().y + m_yVel);
+        rectangle.setPosition(rectangle.getPosition().x + m_xVel, rectangle.getPosition().y + m_yVel);
         text.setPosition(text.getPosition().x + m_xVel, text.getPosition().y + m_yVel);
     }
 
     void detectCol(int width, int height) 
     {
-        if( (circle.getPosition().x >= (width - (m_size*2) )) || circle.getPosition().x < 0) 
+        if( (rectangle.getPosition().x >= (width - m_size.x)) || rectangle.getPosition().x < 0) 
         {
             m_xVel *= -1;
         }
 
-        if( (circle.getPosition().y >= (height - (m_size*2) )) || circle.getPosition().y < 0) 
+        if( (rectangle.getPosition().y >= (height - m_size.y)) || rectangle.getPosition().y < 0) 
         {
             m_yVel *= -1;
         }
 
       
 
-
     }
 
 
 
 private:
-    sf::CircleShape circle;
+    sf::RectangleShape rectangle;
     sf::Text text; 
     sf::Font myFont;
 };
